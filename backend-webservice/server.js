ab6f471b43abf20fbdcbcf164382c944fab2a9c0
DCROWTech <94960389+DCROWTech@users.noreply.github.com>
2022-01-13 20:48:48 +0100

    added server name generation and modified turtle code to send data

--- backend-webservice/server.js
+++ backend-webservice/server.js
@@ -6,7 +6,8 @@ const serverPort = 3000,
     app = express(),
     server = http.createServer(app),
     WebSocket = require("ws"),
-    websocketServer = new WebSocket.Server({ server });
+    websocketServer = new WebSocket.Server({ server }),
+    nameGenerator = require("fantasy-name-generator");
 
 //when a websocket connection is established
 websocketServer.on('connection', (webSocketClient) => {
@@ -17,11 +18,14 @@ websocketServer.on('connection', (webSocketClient) => {
     //when a message is received
     webSocketClient.on('message', (message) => {
         //for each websocket client
-        try{
-            JSON.parse(message.toString("utf-8"))
-        console.log(JSON.parse(message.toString("utf-8")))
-        console.log(JSON.parse(message.toString("utf-8")).test)}
-        catch(e){
+        try {
+            let payload = JSON.parse(message.toString("utf-8"))
+
+            if (payload.name == "nil") {
+                console.log("Turtle has no name. Generating...")
+                console.log("Turtle with id " + payload.id + " has now name: " + nameGenerator.nameByRace("dragon", { gender: "male" }))
+            }
+        } catch (e) {
             console.log(e)
         }
         webSocketClient.send("You just sent " + message)
