40c58ea45d6b825326ec76902bb58b60345e258f
sebastianciocarlan <sebastianciocarlan@gmail.com>
2022-01-14 15:16:12 +0100

    delete unnecesary data

--- backend-webservice/databasecontroller.js
+++
@@ -1,3 +0,0 @@
-export function connectDatabase(mongoose){
-    mongoose.connect("mongodb+srv://cyriac:Arquimedes14!@tweakedturtledatabase.esedy.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
-}
