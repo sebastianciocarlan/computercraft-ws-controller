2c3ebfdde581d3de98b270dfac3a67f7b26cc2b6
DCROWTech <94960389+DCROWTech@users.noreply.github.com>
2022-01-14 12:17:20 +0100

    modify lua

--- turtle-service/turtle.lua
+++ turtle-service/turtle.lua
@@ -1,4 +1,5 @@
 WS = false
+
 while not WS do
     print("Trying to connect...")
     WS = http.websocket("ws://localhost:3000")
@@ -12,6 +13,25 @@ end
 term.clear()
 term.setCursorPos(1,1)
 print("Connected")
+if(gps.locate() == not nil) then
+    local x,y,z = gps.locate()
+    local computerID = os.getComputerID()
+    local computerName = "nil"
+    if os.getComputerLabel() == not nil then
+        computerName = os.getComputerLabel()
+    end
+  
+    
+    WS.send('{"type":"turtle", "position":{"x":'..x..',"y":'..y..',"z":'..z..'},"id":'..computerID..',"name":"'..computerName..'"}')    
+else
+    local computerID = os.getComputerID()
+    local computerName = "nil"
+    if os.getComputerLabel() == not nil then
+        computerName = os.getComputerLabel()
+    end
+    WS.send('{"type":"turtle", "position":"null","id":'..computerID..',"name":"'..computerName..'"}')
+end
+
 while true do
     print("Awaiting for instructions via Websocket")
     local response = WS.receive(30)
